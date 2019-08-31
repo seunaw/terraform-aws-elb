e6299b43f9a8687a749245f837cf5bf36469de58
Seun Wright <seunaw@gmail.com>
2019-08-30 20:37:15 -0700

    added default value to healthcheck

--- modules/elb/main.tf
+++ modules/elb/main.tf
@@ -33,11 +33,11 @@ resource "aws_elb" "this" {
   }
 
   health_check {
-    healthy_threshold   = lookup(var.health_check, "healthy_threshold")
-    unhealthy_threshold = lookup(var.health_check, "unhealthy_threshold")
-    target              = lookup(var.health_check, "target")
-    interval            = lookup(var.health_check, "interval")
-    timeout             = lookup(var.health_check, "timeout")
+    healthy_threshold   = lookup(var.health_check, "healthy_threshold", null)
+    unhealthy_threshold = lookup(var.health_check, "unhealthy_threshold", null)
+    target              = lookup(var.health_check, "target", null)
+    interval            = lookup(var.health_check, "interval", null)
+    timeout             = lookup(var.health_check, "timeout", null)
   }
 
   tags = merge(
