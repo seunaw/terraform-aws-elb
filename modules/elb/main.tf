f0be0f519caa52f72c6664c4e6b3b6b441261715
Seun Wright <seunaw@gmail.com>
2019-10-02 16:39:19 -0700

    Fixes

--- modules/elb/main.tf
+++ modules/elb/main.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "this" {
-  count = !var.network_lb ? 1 : 0
+  #count = !var.network_lb ? 1 : 0
 
   name        = var.name
   name_prefix = var.name_prefix
@@ -53,48 +53,48 @@ resource "aws_elb" "this" {
   )
 }
 
-resource "aws_lb" "this" {
-  count = var.network_lb ? 1 : 0
+# resource "aws_lb" "this" {
+#   count = var.network_lb ? 1 : 0
 
-  name        = var.name
-  name_prefix = var.name_prefix
+#   name        = var.name
+#   name_prefix = var.name_prefix
 
-  subnets         = var.subnets
-  internal        = var.internal
-  security_groups = var.security_groups
+#   subnets         = var.subnets
+#   internal        = var.internal
+#   security_groups = var.security_groups
 
-  load_balancer_type = "network"
+#   load_balancer_type = "network"
 
-  cross_zone_load_balancing   = var.cross_zone_load_balancing
-  idle_timeout                = var.idle_timeout
-  connection_draining         = var.connection_draining
-  connection_draining_timeout = var.connection_draining_timeout
+#   cross_zone_load_balancing   = var.cross_zone_load_balancing
+#   idle_timeout                = var.idle_timeout
+#   connection_draining         = var.connection_draining
+#   connection_draining_timeout = var.connection_draining_timeout
 
-  dynamic "listener" {
-    for_each = var.listener
-    content {
-      instance_port      = listener.value.instance_port
-      instance_protocol  = listener.value.instance_protocol
-      lb_port            = listener.value.lb_port
-      lb_protocol        = listener.value.lb_protocol
-      ssl_certificate_id = lookup(listener.value, "ssl_certificate_id", null)
-    }
-  }
+#   dynamic "listener" {
+#     for_each = var.listener
+#     content {
+#       instance_port      = listener.value.instance_port
+#       instance_protocol  = listener.value.instance_protocol
+#       lb_port            = listener.value.lb_port
+#       lb_protocol        = listener.value.lb_protocol
+#       ssl_certificate_id = lookup(listener.value, "ssl_certificate_id", null)
+#     }
+#   }
 
-  dynamic "access_logs" {
-    for_each = length(keys(var.access_logs)) == 0 ? [] : [var.access_logs]
-    content {
-      bucket        = access_logs.value.bucket
-      bucket_prefix = lookup(access_logs.value, "bucket_prefix", null)
-      interval      = lookup(access_logs.value, "interval", null)
-      enabled       = lookup(access_logs.value, "enabled", true)
-    }
-  }
+#   dynamic "access_logs" {
+#     for_each = length(keys(var.access_logs)) == 0 ? [] : [var.access_logs]
+#     content {
+#       bucket        = access_logs.value.bucket
+#       bucket_prefix = lookup(access_logs.value, "bucket_prefix", null)
+#       interval      = lookup(access_logs.value, "interval", null)
+#       enabled       = lookup(access_logs.value, "enabled", true)
+#     }
+#   }
 
-  tags = merge(
-    var.tags,
-    {
-      "Name" = format("%s", var.name)
-    },
-  )
-}
+#   tags = merge(
+#     var.tags,
+#     {
+#       "Name" = format("%s", var.name)
+#     },
+#   )
+# }
